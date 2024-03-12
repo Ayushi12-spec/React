2613afe2a177085d0fa132a281e2189703281811
Ayushi12-spec <ayushiwantoo@gmail.com>
2024-03-12 12:50:50 +0530

    Making components readble and smaller

---
+++ src/components/ExpenseDetail.jsx
@@ -0,0 +1,11 @@
+import "./ExpenseItem.css";
+
+export default function ExpenseDetails(props) {
+  return (
+    <>
+      <h2>{props.title}</h2>
+      <h2>{props.location}</h2>
+      <div className="expense-item__price">{props.amount}</div>
+    </>
+  );
+}
